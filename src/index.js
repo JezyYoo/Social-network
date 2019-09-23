9664f1981ffa978ecb4f6e125e1d708152a406ea
JezyYoo <volchevdanya@gmail.com>
2019-09-23 10:06:53 +0300

    adding state. OOP

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import * as serviceWorker from './serviceWorker';
-import {renderEntireTree} from "./render";
-import state from "./redux/state";
-import {unregister} from "./serviceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import store from "./redux/state";
 
-renderEntireTree(state);
+
+let renderEntireTree = ()=>
+{
+    ReactDOM.render(
+        <App
+            state={store.getState()}
+            addPost={store.addPost.bind(store)}
+            changeNewPostText={store.changeNewPostText.bind(store)}/> ,
+        document.getElementById('root'));
+}
+
+renderEntireTree();
+store.subscribe(renderEntireTree);
 
 
 
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,47 +1,56 @@
-import {renderEntireTree} from "../render";
+let store ={
+    _state :{
+        dialogsPage:{
+            dialogsData: [
+                {id:1, name:'Angrey'},
+                {id:2, name:'Petya'},
+                {id:3, name:'Sveta'},
+            ],
+            messagesData: [
+                {id:1, name:'LOL'},
+                {id:2, name:'Funny message'},
+                {id:3, name:'Not funny message'},
+            ],
+        },
+        profilePage:{
+            postsData: [
+                {id:1, msg:"Hello wordl",likes:'25'},
+                {id:2, msg:"It's my life",likes:'2'},
+                {id:3, msg:"React is so good",likes:'6'},
+                {id:4, msg:"Fair enough!",likes:'13'},
+            ],
+            newPostText:'aaaaaa'
+        }
 
-let state ={
-    dialogsPage:{
-        dialogsData: [
-            {id:1, name:'Angrey'},
-            {id:2, name:'Petya'},
-            {id:3, name:'Sveta'},
-        ],
-        messagesData: [
-            {id:1, name:'LOL'},
-            {id:2, name:'Funny message'},
-            {id:3, name:'Not funny message'},
-        ],
     },
-    profilePage:{
-        postsData: [
-            {id:1, msg:"Hello wordl",likes:'25'},
-            {id:2, msg:"It's my life",likes:'2'},
-            {id:3, msg:"React is so good",likes:'6'},
-            {id:4, msg:"Fair enough!",likes:'13'},
-        ],
-        newPostText:'aaaaaa'
+
+    getState(){
+        return this._state;
+    },
+
+    addPost(){
+        this._state.profilePage.postsData.push(
+            {
+                id:5,
+                msg:this._state.profilePage.newPostText,
+                likes:'0'
+            })
+        this._state.profilePage.newPostText ='';
+        this.renderEntireTree(this._state);
+    },
+
+    changeNewPostText(text){
+        this._state.profilePage.newPostText = text;
+        this.renderEntireTree(this._state);
+
+    },
+
+    renderEntireTree(){},
+
+    subscribe(observer){
+        this.renderEntireTree = observer;
     }
-
 }
 
-export let addPost = ()=>{
-    state.profilePage.postsData.push(
-        {
-            id:5,
-            msg:state.profilePage.newPostText,
-            likes:'0'
-        })
-    state.profilePage.newPostText ='';
-    renderEntireTree(state);
-}
-
-export let changeNewPostText = (text) =>{
-    state.profilePage.newPostText = text;
-    renderEntireTree(state);
-
-}
-
-
-
-export default state;
+window.sotre = store;
+export default store;
