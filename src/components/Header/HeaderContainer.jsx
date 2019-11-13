a640e7ff08d05ca33ad4465e987aec5f411dbc78
JezyYoo <volchevdanya@gmail.com>
2019-11-13 14:38:55 +0200

    authorization with validation

---
+++ src/components/Header/HeaderContainer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import cl from './Header.module.css';
+import {NavLink} from "react-router-dom";
+
+const Header = (props) => {
+    return (
+        <div className={`col-md-12 ${cl.header}`}>
+            <header >
+                <img alt="" src="https://png.pngtree.com/element_pic/00/16/07/115783931601b5c.jpg" width="100px;"></img>
+            </header>
+
+            <NavLink className={cl.log} to='/login'>Login</NavLink>
+        </div>
+    )
+}
+
+export default Header;
